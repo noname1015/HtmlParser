b451ab2980964446ff681560f54ce2621650713f
noname1015 <noname_1015@hotmail.com>
2023-03-09 11:54:13 +0800

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace HtmlParser
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.txtOutput = new System.Windows.Forms.TextBox();
+            this.btnConvert = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtInput
+            // 
+            this.txtInput.Location = new System.Drawing.Point(13, 40);
+            this.txtInput.Multiline = true;
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(299, 595);
+            this.txtInput.TabIndex = 0;
+            // 
+            // txtOutput
+            // 
+            this.txtOutput.Location = new System.Drawing.Point(402, 40);
+            this.txtOutput.Multiline = true;
+            this.txtOutput.Name = "txtOutput";
+            this.txtOutput.Size = new System.Drawing.Size(318, 588);
+            this.txtOutput.TabIndex = 1;
+            // 
+            // btnConvert
+            // 
+            this.btnConvert.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnConvert.Location = new System.Drawing.Point(319, 289);
+            this.btnConvert.Name = "btnConvert";
+            this.btnConvert.Size = new System.Drawing.Size(77, 59);
+            this.btnConvert.TabIndex = 2;
+            this.btnConvert.Text = "Convert";
+            this.btnConvert.UseVisualStyleBackColor = true;
+            this.btnConvert.Click += new System.EventHandler(this.btnConvert_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(39, 18);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Input";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(399, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(87, 18);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Html Output";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(732, 640);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnConvert);
+            this.Controls.Add(this.txtOutput);
+            this.Controls.Add(this.txtInput);
+            this.Name = "Form1";
+            this.Text = "HtmlParser";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.TextBox txtOutput;
+        private System.Windows.Forms.Button btnConvert;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+using System.Xml.Linq;
+
+namespace HtmlParser
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnConvert_Click(object sender, EventArgs e)
+        {
+            
+            string html = HtmlContentParser.ParseTable(txtInput.Text);
+
+            txtOutput.Text = html;
+        }
+    }
+
+   
+}
+public class HtmlObjLine
+{
+    private string _tag;
+    private int _level;
+
+    public string Tag
+    {
+        get { return _tag; }   // get method
+        set { _tag = value; }  // set method
+    }
+
+    public int Level
+    {
+        get { return _level; }   // get method
+        set { _level = value; }  // set method
+    }
+}
+public class HtmlContentParser : HtmlObjLine
+{
+
+
+
+    public static string ParseTable(string input)
+    {
+        StringBuilder builder = new StringBuilder();
+        Stack<HtmlObjLine> stack = new Stack<HtmlObjLine>();
+
+        foreach (string line in input.Split('\n'))
+        {
+            int level = line.Length - line.TrimStart(' ').Length;
+
+            // Remove indentation and trim any trailing spaces
+            string content = line.TrimStart(' ').TrimEnd();
+
+            string tag = content.Split(' ')[0];
+
+
+            if (level == 0)
+            {
+                // Top-level element, should be "table"
+                //stack.Push("table");
+                builder.AppendLine("<table>");
+                HtmlObjLine htmlLine = new HtmlObjLine();
+                htmlLine.Tag = content;
+                htmlLine.Level = level;
+                stack.Push(htmlLine);
+            }
+            else
+            {
+                // Determine the tag name based on the content of the line
+
+
+                HtmlObjLine lastObjLine = stack.Peek();
+
+                if (level > lastObjLine.Level)
+                {
+                    builder.AppendLine(new string(' ', level) + $"<{tag}>");
+                    HtmlObjLine newhtmlLine = new HtmlObjLine();
+                    newhtmlLine.Level = level;
+                    newhtmlLine.Tag = tag;
+
+                    stack.Push(newhtmlLine);
+                    // Handle text content (if any)
+                    if (content.Length > tag.Length)
+                    {
+                        string text = content.Substring(tag.Length + 1);
+                        builder.AppendLine(new string(' ', level + 2) + text);
+
+                    }
+                }
+                else if (level == lastObjLine.Level)
+                {
+
+                    stack.Pop();
+                    builder.AppendLine(new string(' ', lastObjLine.Level) + $"</{lastObjLine.Tag}>");
+                    builder.AppendLine(new string(' ', level) + $"<{tag}>");
+
+                    // Handle text content (if any)
+                    if (content.Length > tag.Length)
+                    {
+                        string text = content.Substring(tag.Length + 1);
+                        builder.AppendLine(new string(' ', level + 2) + text);
+                        builder.AppendLine(new string(' ', level) + $"</{tag}>");
+
+                    }
+                    HtmlObjLine newhtmlLine = new HtmlObjLine();
+                    newhtmlLine.Level = level;
+                    newhtmlLine.Tag = tag;
+                    stack.Push(newhtmlLine);
+                }
+                else
+                {
+                    stack.Pop();
+                    builder.AppendLine(new string(' ', lastObjLine.Level) + $"</{lastObjLine.Tag}>");
+
+                    //Close higher or same level previous tag before proceed
+                    while (lastObjLine.Level >= level)
+                    {
+                        HtmlObjLine currhtmlLine = stack.Pop();
+                        builder.AppendLine(new string(' ', currhtmlLine.Level) + $"</{currhtmlLine.Tag}>");
+                        lastObjLine = stack.Peek();
+                    }
+
+                    builder.AppendLine(new string(' ', level) + $"<{tag}>");
+                    HtmlObjLine newhtmlLine = new HtmlObjLine();
+                    newhtmlLine.Level = level;
+                    newhtmlLine.Tag = tag;
+                    stack.Push(newhtmlLine);
+                }
+               
+            }
+
+           
+
+        }
+
+        // Close any remaining tags
+        while (stack.Count > 0)
+        {
+            HtmlObjLine currhtmlLine = stack.Pop();
+            builder.AppendLine(new string(' ', currhtmlLine.Level) + $"</{currhtmlLine.Tag}>");
+        }
+
+        return builder.ToString();
+    }
+
+ 
+
+
+}
+
+
